3cee7e244067a50af97fa5576805ff57ca08da40
bito999p-ctrl <bito.999.p@gmail.com>
2026-08-24 00:30:53 +0900

    Reorder artists catalog: 01 Bito, 02 Valotoa, 03 Hizumi, 04 pophoper, 05 Stray Glitch Monkeys, 06 day end night, 07 Asymptote, 08 RUPTURE, 09 Sonografica, 10 THE METROPOLITANS

--- script.js
+++ script.js
@@ -4,15 +4,6 @@
 
 const artists = [
     {
-        id: "valotoa", name: "valotoa",
-        links: [
-            { type: "youtube", url: "https://youtube.com/playlist?list=PLOb7KvIzm6NE&si=2InPpcagbd-i9dKD", label: "YouTube" },
-            { type: "suno", url: "https://suno.com/playlist/c95d6dd3-c6ee-41a8-9a9c-8213e6b07dcb", label: "Suno" }
-        ],
-        spotifyUrls: [],
-        youtubeUrls: ["https://youtube.com/playlist?list=PLOb7KvIzm6NE&si=2InPpcagbd-i9dKD"]
-    },
-    {
         id: "bito", name: "Bito",
         links: [
             { type: "twitter", url: "https://x.com/BitoCraftedTune", label: "X" },
@@ -26,15 +17,13 @@ const artists = [
         youtubeUrls: ["https://youtube.com/playlist?list=PLxpRgysXp3GlUaHN7GwWZheqxz5XCbdzL&si=YVcTUFljO9vOOcwF"]
     },
     {
-        id: "pophoper", name: "pophoper",
+        id: "valotoa", name: "valotoa",
         links: [
-            { type: "spotify", url: "https://open.spotify.com/intl-ja/artist/5fejGOb2AqHlneXYKJVwF7?si=X175bF7MTsO5obPIIs_oEA", label: "Spotify" },
-            { type: "youtube", url: "https://youtube.com/playlist?list=PLxpRgysXp3GnP710vJ5DwL8BC9KyG0AlN&si=B3S3Y9AdjN69LqUV", label: "YouTube" },
-            { type: "suno", url: "https://suno.com/playlist/0635e884-f792-47ac-91c4-c334b605ba0a", label: "Suno" },
-            { type: "aisa", url: "https://aisa.radioalps.com/music/artist/pophoper", label: "AISA RADIO" }
+            { type: "youtube", url: "https://youtube.com/playlist?list=PLOb7KvIzm6NE&si=2InPpcagbd-i9dKD", label: "YouTube" },
+            { type: "suno", url: "https://suno.com/playlist/c95d6dd3-c6ee-41a8-9a9c-8213e6b07dcb", label: "Suno" }
         ],
-        spotifyUrls: ["https://open.spotify.com/intl-ja/artist/5fejGOb2AqHlneXYKJVwF7?si=X175bF7MTsO5obPIIs_oEA"],
-        youtubeUrls: ["https://youtube.com/playlist?list=PLxpRgysXp3GnP710vJ5DwL8BC9KyG0AlN&si=B3S3Y9AdjN69LqUV"]
+        spotifyUrls: [],
+        youtubeUrls: ["https://youtube.com/playlist?list=PLOb7KvIzm6NE&si=2InPpcagbd-i9dKD"]
     },
     {
         id: "hizumi", name: "歪み歪み -hizumi yugami-",
@@ -48,6 +37,17 @@ const artists = [
         youtubeUrls: ["https://youtube.com/playlist?list=PLxpRgysXp3Gm6OgQmHL3bSaAfFxF-DK7d&si=H9atE_w5lqlmEo6c"]
     },
     {
+        id: "pophoper", name: "pophoper",
+        links: [
+            { type: "spotify", url: "https://open.spotify.com/intl-ja/artist/5fejGOb2AqHlneXYKJVwF7?si=X175bF7MTsO5obPIIs_oEA", label: "Spotify" },
+            { type: "youtube", url: "https://youtube.com/playlist?list=PLxpRgysXp3GnP710vJ5DwL8BC9KyG0AlN&si=B3S3Y9AdjN69LqUV", label: "YouTube" },
+            { type: "suno", url: "https://suno.com/playlist/0635e884-f792-47ac-91c4-c334b605ba0a", label: "Suno" },
+            { type: "aisa", url: "https://aisa.radioalps.com/music/artist/pophoper", label: "AISA RADIO" }
+        ],
+        spotifyUrls: ["https://open.spotify.com/intl-ja/artist/5fejGOb2AqHlneXYKJVwF7?si=X175bF7MTsO5obPIIs_oEA"],
+        youtubeUrls: ["https://youtube.com/playlist?list=PLxpRgysXp3GnP710vJ5DwL8BC9KyG0AlN&si=B3S3Y9AdjN69LqUV"]
+    },
+    {
         id: "stray", name: "Stray Glitch Monkeys",
         links: [
             { type: "spotify", url: "https://open.spotify.com/intl-ja/artist/280n7G2T6dmFkCRs8JFMeX?si=v1hsCKO3TauIOwhjwdT6ng", label: "Spotify" },
@@ -58,15 +58,22 @@ const artists = [
         youtubeUrls: ["https://youtube.com/playlist?list=PLxpRgysXp3GlaHKI8Wz0WWATs5SZFC6o4&si=3ffy2M9F2ouSJg32"]
     },
     {
-        id: "metropolitans", name: "THE METROPOLITANS",
+        id: "dayendnight", name: "day end night",
         links: [
-            { type: "spotify", url: "https://open.spotify.com/intl-ja/artist/5lSsV9mEnzTwpDzOSWqPiQ", label: "Spotify" },
-            { type: "youtube", url: "https://youtube.com/playlist?list=PLxpRgysXp3GnkxK3lm_cwHLD_alvdc_3t&si=d-KLo6zSzbX7Y4VP", label: "YouTube" },
-            { type: "suno", url: "https://suno.com/playlist/f59d229a-79fc-4b48-b36a-1efbac94175f", label: "Suno" },
-            { type: "aisa", url: "https://aisa.radioalps.com/music/artist/the-metropolitans", label: "AISA RADIO" }
+            { type: "youtube", url: "https://youtube.com/playlist?list=PLxpRgysXp3GmFmY4eSMSeAcFM3qjNN4gE&si=3TtpYPlWD63gZ2B3i", label: "YouTube" },
+            { type: "suno", url: "https://suno.com/playlist/c685d560-7182-4a81-b430-b4bf33bcfd4f", label: "Suno" }
         ],
-        spotifyUrls: ["https://open.spotify.com/intl-ja/artist/5lSsV9mEnzTwpDzOSWqPiQ"],
-        youtubeUrls: ["https://youtube.com/playlist?list=PLxpRgysXp3GnkxK3lm_cwHLD_alvdc_3t&si=d-KLo6zSzbX7Y4VP"]
+        spotifyUrls: [],
+        youtubeUrls: ["https://youtube.com/playlist?list=PLxpRgysXp3GmFmY4eSMSeAcFM3qjNN4gE&si=3TtpYPlWD63gZ2B3i"]
+    },
+    {
+        id: "asymptote", name: "Asymptote",
+        links: [
+            { type: "youtube", url: "https://youtube.com/playlist?list=PLxpRgysXp3GlPvzX5hzWu4YxyPXrnriyM&si=OeHh87ijcXOVcxWi", label: "YouTube" },
+            { type: "suno", url: "https://suno.com/playlist/c95d6dd3-c6ee-41a8-9a9c-8213e6b07dcb", label: "Suno" }
+        ],
+        spotifyUrls: [],
+        youtubeUrls: ["https://youtube.com/playlist?list=PLxpRgysXp3GlPvzX5hzWu4YxyPXrnriyM&si=OeHh87ijcXOVcxWi"]
     },
     {
         id: "rupture", name: "RUPTURE",
@@ -79,24 +86,6 @@ const artists = [
         youtubeUrls: ["https://youtube.com/playlist?list=PLxpRgysXp3Gmw6VX82wfxnStZ8lGeuNp9&si=mujfGGlFHVWnC_kV"]
     },
     {
-        id: "asymptote", name: "Asymptote",
-        links: [
-            { type: "youtube", url: "https://youtube.com/playlist?list=PLxpRgysXp3GlPvzX5hzWu4YxyPXrnriyM&si=OeHh87ijcXOVcxWi", label: "YouTube" },
-            { type: "suno", url: "https://suno.com/playlist/c95d6dd3-c6ee-41a8-9a9c-8213e6b07dcb", label: "Suno" }
-        ],
-        spotifyUrls: [],
-        youtubeUrls: ["https://youtube.com/playlist?list=PLxpRgysXp3GlPvzX5hzWu4YxyPXrnriyM&si=OeHh87ijcXOVcxWi"]
-    },
-    {
-        id: "dayendnight", name: "day end night",
-        links: [
-            { type: "youtube", url: "https://youtube.com/playlist?list=PLxpRgysXp3GmFmY4eSMSeAcFM3qjNN4gE&si=3TtpYPlWD63gZ2B3i", label: "YouTube" },
-            { type: "suno", url: "https://suno.com/playlist/c685d560-7182-4a81-b430-b4bf33bcfd4f", label: "Suno" }
-        ],
-        spotifyUrls: [],
-        youtubeUrls: ["https://youtube.com/playlist?list=PLxpRgysXp3GmFmY4eSMSeAcFM3qjNN4gE&si=3TtpYPlWD63gZ2B3i"]
-    },
-    {
         id: "sonografica-artist", name: "Sonografica",
         links: [
             { type: "youtube", url: "https://youtube.com/playlist?list=PLLOIHKwdVqbE&si=k-m1OGcNYQMMIxkj", label: "YouTube" },
@@ -104,6 +93,17 @@ const artists = [
         ],
         spotifyUrls: [],
         youtubeUrls: ["https://youtube.com/playlist?list=PLLOIHKwdVqbE&si=k-m1OGcNYQMMIxkj"]
+    },
+    {
+        id: "metropolitans", name: "THE METROPOLITANS",
+        links: [
+            { type: "spotify", url: "https://open.spotify.com/intl-ja/artist/5lSsV9mEnzTwpDzOSWqPiQ", label: "Spotify" },
+            { type: "youtube", url: "https://youtube.com/playlist?list=PLxpRgysXp3GnkxK3lm_cwHLD_alvdc_3t&si=d-KLo6zSzbX7Y4VP", label: "YouTube" },
+            { type: "suno", url: "https://suno.com/playlist/f59d229a-79fc-4b48-b36a-1efbac94175f", label: "Suno" },
+            { type: "aisa", url: "https://aisa.radioalps.com/music/artist/the-metropolitans", label: "AISA RADIO" }
+        ],
+        spotifyUrls: ["https://open.spotify.com/intl-ja/artist/5lSsV9mEnzTwpDzOSWqPiQ"],
+        youtubeUrls: ["https://youtube.com/playlist?list=PLxpRgysXp3GnkxK3lm_cwHLD_alvdc_3t&si=d-KLo6zSzbX7Y4VP"]
     }
 ];
 
